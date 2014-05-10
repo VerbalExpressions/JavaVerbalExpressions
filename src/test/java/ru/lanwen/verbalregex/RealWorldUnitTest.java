df2e529677c0b3ee4ebc6dcbe28cd93e4e65834b
MerkushevKirill <lanwen@yandex-team.ru>
2014-05-10 17:16:50 +0400

    add - factory methods for builder (with method to clone)

--- src/test/java/ru/lanwen/verbalregex/RealWorldUnitTest.java
+++ src/test/java/ru/lanwen/verbalregex/RealWorldUnitTest.java
@@ -2,8 +2,9 @@ package ru.lanwen.verbalregex;
 
 import org.junit.Test;
 
+
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -29,5 +30,32 @@ public class RealWorldUnitTest {
         assertThat("Regex doesn't match same regex as in example",
                 testRegex.toString(),
                 equalTo("^(http)(s)?(\\:\\/\\/)(www\\.)?([^\\ ]*)$"));
+    }
+
+    @Test
+    public void staticFabricsRetunSameAsConstructorExpressions() {
+        VerbalExpression regexViaFactory = VerbalExpression.regex().anything().build();
+        VerbalExpression regexViaConstructor = new VerbalExpression.Builder().anything().build();
+
+        assertThat("Factory builder method produce not same as constructor regex",
+                regexViaFactory.toString(), equalTo(regexViaConstructor.toString()));
+    }
+
+    @Test
+    public void clonedBuilderEqualsOriginal() {
+        VerbalExpression.Builder builder = VerbalExpression.regex().anything().addModifier('i');
+        VerbalExpression.Builder clonedBuilder = VerbalExpression.regex(builder);
+
+        assertThat("Cloned builder changed after creating new one",
+                builder.build().toString(), equalTo(clonedBuilder.build().toString()));
+    }
+
+    @Test
+    public void clonedBuilderCantChangeOriginal() {
+        VerbalExpression.Builder builder = VerbalExpression.regex().anything().addModifier('i');
+        VerbalExpression.Builder clonedBuilder = VerbalExpression.regex(builder).endOfLine();
+
+        assertThat("Cloned builder changed after creating new one",
+                builder.build().toString(), not(clonedBuilder.build().toString()));
     }
 }
