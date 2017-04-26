68cc876348d842cd21657bb0151d3cfc51c2743a
Demeter Gabor <demetergabor94@gmail.com>
2017-04-26 20:53:22 +0200

    proba

--- src/main/java/ru/lanwen/verbalregex/VerbalExpression.java
+++ src/main/java/ru/lanwen/verbalregex/VerbalExpression.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+//comment
 public class VerbalExpression {
 
     private final Pattern pattern;
